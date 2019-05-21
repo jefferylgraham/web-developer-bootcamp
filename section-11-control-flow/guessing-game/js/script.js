932ba5a7edf546656bb6966c32b87c4a08a76e6c
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-20 20:53:41 -0400

    add guessing game codealong exercise

---
+++ section-11-control-flow/guessing-game/js/script.js
@@ -0,0 +1,18 @@
+// create secretNumber
+var secretNumber = 4;
+
+//ask user for guess
+var guess = Number(prompt("Guess a number"));
+
+//check if guess is right
+if(guess === secretNumber) {
+  alert("You got it right!");
+}
+//otherwise, check if higher
+else if(guess > secretNumber) {
+  alert("Too High! Guess again");
+}
+//otherwise, check if lower
+else {
+  alert("Too low! Guess again");
+}
