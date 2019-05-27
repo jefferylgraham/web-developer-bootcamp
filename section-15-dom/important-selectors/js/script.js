fbd24f0354fb9c0e86a70319a5329d6b9e485799
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-27 17:23:10 -0400

    add selector exercise

---
+++ section-15-dom/important-selectors/js/script.js
@@ -0,0 +1,9 @@
+var tag = document.getElementById("highlighted");
+
+var tags = document.getElementsByClassName("bolded");
+
+var tags2 = document.getElementsByTagName("li");
+
+var highlighted = document.querySelector("#highlighted");
+
+var h1Tags = document.querySelectorAll("h1");
