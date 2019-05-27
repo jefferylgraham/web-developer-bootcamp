11dab7ba33e6cf31a41d1458b874e6d78fb6df13
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-27 19:05:36 -0400

    add dom manipulation exercises

---
+++ section-15-dom/dom-manipulation/js/script.js
@@ -0,0 +1,17 @@
+// var h1 = document.querySelector("h1");
+
+// h1.style.color = "yellow";
+// h1.style.border = "5px solid pink";
+
+// var p = document.querySelector("p");
+
+// p.classList.add("big");
+
+var p = document.getElementsByTagName("p")[0];
+
+
+var ul = document.querySelector("ul");
+
+var img1 = document.getElementsByTagName("img")[0];
+
+var a = document.querySelector("a");
