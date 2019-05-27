b687bfa83264ed14ef9871af5d7f9715bbcd62b9
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-27 17:32:45 -0400

    add selector exercise solution

---
+++ section-15-dom/selector-exercise/js/script.js
@@ -0,0 +1,9 @@
+var solution1 = document.getElementById("first");
+
+var solution2 = document.getElementsByClassName("special")[0];
+
+var solution3 = document.getElementsByTagName("p")[0];
+
+var solution4 = document.querySelector("#first");
+
+var solution5 = document.querySelectorAll(".special")[0];
