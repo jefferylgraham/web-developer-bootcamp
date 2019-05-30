84c2fcefa627645ba3d52e1f45742525a2186b67
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-30 18:30:30 -0400

    add todo list exercise

---
+++ section-16-advanced-dom-manipulation/todo-list/js/script.js
@@ -0,0 +1,21 @@
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("mouseover", function() {
+    this.classList.add("selected");
+  });
+  lis[i].addEventListener("mouseout", function() {
+    this.classList.remove("selected");
+  });
+  lis[i].addEventListener("click", function() {
+    this.classList.toggle("done");
+  });
+}
+
+// firstLI.addEventListener("mousemove", function() {
+//   firstLI.style.color = "green";
+// });
+
+// firstLI.addEventListener("mouseout", function() {
+//   firstLI.style.color = "black";
+// });
