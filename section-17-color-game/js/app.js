4b9934da9569a351bb704b113f48890e51d9e63e
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-06-01 14:25:28 -0400

    randomize picked color

--- section-17-color-game/js/app.js
+++ section-17-color-game/js/app.js
@@ -7,23 +7,36 @@ var colors = [
   "rgb(255, 0, 255)"
 ];
 
-var goalColor = colors[3];
+var pickedColor = pickColor();
 
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
-colorDisplay.textContent = goalColor;
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function() {
     var clickedColor = this.style.backgroundColor;
-    if(clickedColor === goalColor) {
-      this.style.backgroundColor = "#232323"
+    if(clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     }
     else {
-      alert("wrong");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
 }
 
+function changeColors(color) {
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random];
+}
