3fcbebc6591b02602410a295fce76ab45fb3aecf
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-06-01 14:44:50 -0400

    randomize square colors

--- section-17-color-game/js/app.js
+++ section-17-color-game/js/app.js
@@ -1,17 +1,9 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 000)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var pickedColor = pickColor();
-
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,6 +13,7 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     if(clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      h1.style.backgroundColor = clickedColor;
       changeColors(clickedColor);
     }
     else {
@@ -39,4 +32,19 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length)
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+  for(var i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")"
 }
