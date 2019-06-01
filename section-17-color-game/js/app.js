728c3c10a9df51989c388aa3266afa7b0d807860
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-06-01 19:14:35 -0400

    add styling

--- section-17-color-game/js/app.js
+++ section-17-color-game/js/app.js
@@ -25,7 +25,7 @@ easyBtn.addEventListener("click", function() {
       squares[i].style.display = "none";
     }
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 hardBtn.addEventListener("click", function() {
@@ -39,7 +39,7 @@ hardBtn.addEventListener("click", function() {
     squares[i].style.backgroundColor = colors[i];
     squares[i].style.display = "block";
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 resetButton.addEventListener("click", function() {
@@ -53,7 +53,7 @@ resetButton.addEventListener("click", function() {
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
